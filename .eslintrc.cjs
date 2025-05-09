208a0fe0507efc8fe326a7d7be62aeaaa5db26ec
Adnan Lakhani <adnan@lakhaniproductions.com>
2025-05-09 13:04:05 -0400

    updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,6 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  typescript: {
-    ignoreBuildErrors: true
-  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
