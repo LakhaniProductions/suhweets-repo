9776876daf1eaa0c1d78772f79582cf0b0187d50
Adnan Lakhani <adnan@lakhaniproductions.com>
2025-05-14 13:07:32 -0400

    Update Jumbotron.tsx

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -1,13 +1,8 @@
 import { Link } from "react-router-dom";
 import "./jumbotron.css";
 import { JumbotronProps } from "./Jumbotron.types";
-import { useEffect, useState } from "react";
-import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const Jumbotron = (props: JumbotronProps) => {
-  const { width } = useWindowDimensions();
-  const [showShrtBdyTxt, setShowShrtBdyTxt] = useState(false);
-  const [shrtBdyTxt, setShrtBdyTxt] = useState<string>("");
   let contentObj;
   {
     props.content.map((content) =>
@@ -21,28 +16,12 @@ const Jumbotron = (props: JumbotronProps) => {
       })
     );
   }
-  useEffect(() => {
-    width <= 926 ? setShowShrtBdyTxt(true) : setShowShrtBdyTxt(false);
-  }, [width]);
 
-  useEffect(() => {
-    props.content.map((content) =>
-      content.linkFor === props.jumbotron &&
-      props.jumbotron === "wedding" &&
-      showShrtBdyTxt
-        ? setShrtBdyTxt(content.bodytxt.slice(11))
-        : ""
-    );
-  }, [showShrtBdyTxt]);
   return (
     <div className="jumbotron">
       <h2 className="sub-heading-home">{contentObj!.subhead}</h2>
       <h1 className="heading-home">{contentObj!.heading}</h1>
-      <p className="body-text-home">
-        {props.jumbotron === "wedding" && showShrtBdyTxt
-          ? shrtBdyTxt
-          : contentObj!.bodytxt}
-      </p>
+      <p className="body-text-home">{contentObj!.bodytxt}</p>
 
       <Link className="home-pg-links" to={contentObj!.galleryLink}>
         view gallery
