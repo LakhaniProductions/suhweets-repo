1b63367fc6335d02f45688d1e92e870aceb7356a
Adnan Lakhani <adnan@lakhaniproductions.com>
2025-05-14 14:15:56 -0400

    Update ServingCards.tsx

--- src/components/ServingCards/ServingCards.tsx
+++ src/components/ServingCards/ServingCards.tsx
@@ -9,7 +9,7 @@ const ServingCards = (props: servingCardsProp) => {
   }
   const { setAllGalleryImagesArr } = context;
   const servingImages = Object.values(
-    import.meta.glob("../../img/servingssizes/*.{png,jpg,jpeg,PNG,JPEG}", {
+    import.meta.glob("../../img/servingssizes/*.{png,jpg,jpeg}", {
       eager: true,
       as: "url"
     })
@@ -18,37 +18,37 @@ const ServingCards = (props: servingCardsProp) => {
     {
       diameter: '6" diameter',
       servings: "Serves 12",
-      img: `${servingImages.filter((img) => img.includes("6inch.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6inch.png"))}`,
       category: "1-tier"
     },
     {
       diameter: '7" diameter',
       servings: "Serves 15",
-      img: `${servingImages.filter((img) => img.includes("7inch.png"))}`,
+      img: `${servingImages.find((img) => img.includes("7inch.png"))}`,
       category: "1-tier"
     },
     {
       diameter: '8" diameter',
       servings: "Serves 24",
-      img: `${servingImages.filter((img) => img.includes("8inch.png"))}`,
+      img: `${servingImages.find((img) => img.includes("8inch.png"))}`,
       category: "1-tier"
     },
     {
       diameter: '9" diameter',
       servings: "Serves 30",
-      img: `${servingImages.filter((img) => img.includes("9inch.png"))}`,
+      img: `${servingImages.find((img) => img.includes("9inch.png"))}`,
       category: "1-tier"
     },
     {
       diameter: '10" diameter',
       servings: "Serves 38",
-      img: `${servingImages.filter((img) => img.includes("10inch.png"))}`,
+      img: `${servingImages.find((img) => img.includes("10inch.png"))}`,
       category: "1-tier"
     },
     {
       diameter: '6" & 8" diameters',
       servings: "Serves 36",
-      img: `${servingImages.filter(
+      img: `${servingImages.find(
         (img) => img.includes("6-8.png") && !img.includes("4")
       )}`,
       category: "2-tier"
@@ -56,19 +56,19 @@ const ServingCards = (props: servingCardsProp) => {
     {
       diameter: '6" & 9" diameters',
       servings: "Serves 42",
-      img: `${servingImages.filter((img) => img.includes("6-9.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6-9.png"))}`,
       category: "2-tier"
     },
     {
       diameter: '6" & 10" diameters',
       servings: "Serves 50",
-      img: `${servingImages.filter((img) => img.includes("6-10.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6-10.png"))}`,
       category: "2-tier"
     },
     {
       diameter: '8" & 10" diameters',
       servings: "Serves 62",
-      img: `${servingImages.filter(
+      img: `${servingImages.find(
         (img) =>
           img.includes("8-10.png") && !img.includes("4") && !img.includes("6")
       )}`,
@@ -77,19 +77,19 @@ const ServingCards = (props: servingCardsProp) => {
     {
       diameter: '8" & 12" diameters',
       servings: "Serves 80",
-      img: `${servingImages.filter((img) => img.includes("8-12.png"))}`,
+      img: `${servingImages.find((img) => img.includes("8-12.png"))}`,
       category: "2-tier"
     },
     {
       diameter: '4" 6" & 8" diameters',
       servings: "Serves 44",
-      img: `${servingImages.filter((img) => img.includes("4-6-8.png"))}`,
+      img: `${servingImages.find((img) => img.includes("4-6-8.png"))}`,
       category: "3-tier"
     },
     {
       diameter: '6" 8" & 10" diameters',
       servings: "Serves 74",
-      img: `${servingImages.filter(
+      img: `${servingImages.find(
         (img) => img.includes("6-8-10.png") && !img.includes("4")
       )}`,
       category: "3-tier"
@@ -97,25 +97,25 @@ const ServingCards = (props: servingCardsProp) => {
     {
       diameter: '6" 9" & 12" diameters',
       servings: "Serves 100",
-      img: `${servingImages.filter((img) => img.includes("6-9-12.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6-9-12.png"))}`,
       category: "3-tier"
     },
     {
       diameter: '6" 10" & 14" diameters',
       servings: "Serves 128",
-      img: `${servingImages.filter((img) => img.includes("6-10-14.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6-10-14.png"))}`,
       category: "3-tier"
     },
     {
       diameter: '4" 6" 8" & 10" diameters',
       servings: "Serves 82",
-      img: `${servingImages.filter((img) => img.includes("4-6-8-10.png"))}`,
+      img: `${servingImages.find((img) => img.includes("4-6-8-10.png"))}`,
       category: "4-tier"
     },
     {
       diameter: '6" 8" 10" & 12" diameters',
       servings: "Serves 130",
-      img: `${servingImages.filter((img) =>
+      img: `${servingImages.find((img) =>
         img.includes("6-8-10-12-4-tier.png")
       )}`,
       category: "4-tier"
@@ -123,32 +123,23 @@ const ServingCards = (props: servingCardsProp) => {
     {
       diameter: '6" 9" 12" & 14" diameters',
       servings: "Serves 178",
-      img: `${servingImages.filter((img) => img.includes("6-9-12-14.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6-9-12-14.png"))}`,
       category: "4-tier"
     },
     {
       diameter: '4" 6" 8" 10" & 12" diameters',
       servings: "Serves 138",
-      img: `${servingImages.filter((img) => img.includes("4-6-8-10-12.png"))}`,
+      img: `${servingImages.find((img) => img.includes("4-6-8-10-12.png"))}`,
       category: "5-tier"
     },
     {
       diameter: '6" 8" 10" 12" & 14" diameters',
       servings: "Serves 208",
-      img: `${servingImages.filter((img) => img.includes("6-8-10-12-14.png"))}`,
+      img: `${servingImages.find((img) => img.includes("6-8-10-12-14.png"))}`,
       category: "5-tier"
     }
   ];
   const [secondaryClassName, setSecondaryClassName] = useState("");
-
-  // useEffect(() => {
-  //   const allOptionsOnPage = servingSizeContent.filter(
-  //     (obj: Record<string, string>) => obj.category === props.html
-  //   );
-
-  //   allOptionsOnPage.length === allGalleryImagesArr.length &&
-  //     setShowLoadingGif(false);
-  // }, [allGalleryImagesArr]);
 
   useEffect(() => {
     if (props.html === "1-tier") {
