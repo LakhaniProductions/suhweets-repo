f5a588fda06749f7116ee133ae41c87adcbba3b6
Adnan Lakhani <adnan@lakhaniproductions.com>
2026-01-31 15:48:19 -0500

    homepage updates

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,18 @@
 import { Link } from "react-router-dom";
-import fullLogo from "../../img/suhweetslogotagline.png";
 import logoMark from "../../img/suhweets_logomark.png";
-import wordMark from "../../img/suhweets_wordmark.png";
 import { HeaderProps } from "./Header.types";
 import "./header.css";
 import HamburgerMenuIcon from "../HamburgerMenuIcon/HamburgerMenuIcon";
-import useWindowDimensions from "../../hooks/useWindowDimensions";
-import { useEffect, useState } from "react";
 
 const Header = (props: HeaderProps) => {
-  const { width, height } = useWindowDimensions();
-  const [logoSRC, setLogoSRC] = useState(fullLogo);
-  const [logoClass, setLogoClass] = useState("logo");
-
-  useEffect(() => {
-    if (width <= 848 && width > 600) {
-      setLogoSRC(wordMark);
-      setLogoClass("logo wordmark");
-    } else if (width <= 600) {
-      setLogoSRC(logoMark);
-      setLogoClass("logo logomark");
-    } else {
-      setLogoClass("logo");
-      setLogoSRC(fullLogo);
-    }
-  }, [width, height]);
-
   return (
     <header className="header">
-      <Link to="/" tabIndex={1}>
-        <img src={`${logoSRC}`} alt="" className={`${logoClass}`} />
-      </Link>
-      <HamburgerMenuIcon setMenuFade={props.setMenuFade} />
+      <div className="header-content">
+        <Link to="/" tabIndex={1}>
+          <img src={`${logoMark}`} alt="" className={"logo logomark"} />
+        </Link>
+        <HamburgerMenuIcon setMenuFade={props.setMenuFade} />
+      </div>
     </header>
   );
 };
--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -1,23 +1,42 @@
 import { Link } from "react-router-dom";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+
 import "./jumbotron.css";
+import wedding from "../../img/home-backgrounds/weddingnew.jpg";
+import weddingPort from "../../img/home-backgrounds/weddingport.jpg";
+import { useEffect, useState } from "react";
 
 const Jumbotron = () => {
+  const { width, height } = useWindowDimensions();
+  const [imgSRC, setImgSRC] = useState(wedding);
+  useEffect(() => {
+    if (width <= 660) {
+      setImgSRC(weddingPort);
+    } else {
+      setImgSRC(wedding);
+    }
+  }, [width, height]);
   return (
     <div className="jumbotron">
-      {/* <h2 className="sub-heading-home">weddings</h2> */}
+      <div className="jumbo-txt-box">
+        <h2 className="sub-heading-home">a day you'll </h2>
 
-      <h1 className="heading-home">weddings</h1>
+        <h1 className="heading-home">never forget</h1>
 
-      <p className="body-text-home">
-        Let us help you capture your vision. Browse our cakes and fall in love
-        again!
-      </p>
+        <p className="body-text-home">
+          Let us help you capture your vision. <br></br> Browse our cakes and
+          fall in love again!
+        </p>
+        <div className="jumbo-link-box">
+          <Link className="home-pg-links" to={"/wedding-cakes/wedding/0"}>
+            View Gallery
+          </Link>
 
-      <Link className="home-pg-links" to={"/wedding-cakes/wedding/0"}>
-        View Gallery
-      </Link>
-
-      <p className={`btn-home-line gallery-line`}>&nbsp;</p>
+          <p className={`btn-home-line gallery-line`}>&nbsp;</p>
+        </div>
+      </div>
+      <div className="jumbo-grad"></div>
+      <img src={imgSRC} alt="" />
     </div>
   );
 };
